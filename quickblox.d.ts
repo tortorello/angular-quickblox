ee2b34d31012d2df1f2908421c679992e465c5b6
Victor Tortorello Neto <neto@silicianos.com.br>
2018-01-07 19:39:25 -0200

    A few improvements.

--- quickblox.d.ts
+++ quickblox.d.ts
@@ -7,6 +7,6 @@ export class QuickBlox {
     users: any;
     init(appIdOrToken: number, authKeyOrAppId: string, authSecret: string, configMap?: any);
     createSession(params: string, callback: any);
-    login(params: string, callback: any);
+    login(params: any, callback: any);
     logout(callback: any);
 }
